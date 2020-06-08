b3304cf7d4f23a71b40443db9f199bdc741c5df3
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-06-08 15:53:53 +0300

    Add tests

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -119,7 +119,8 @@
             @foreach ($game['screenshots'] as $screenshot)
             <div>
                 <a href="{{ $screenshot['huge'] }}">
-                    <img src="{{ $screenshot['big'] }}">
+                    <img src="{{ $screenshot['big'] }}" alt="screenshot"
+                        class="hover:opacity-75 transition ease-in-out duration-150">
                 </a>
             </div>
             @endforeach
