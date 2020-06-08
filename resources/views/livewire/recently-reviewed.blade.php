9b0e502ded56af40cd14e9afc3d6a0fbe34225de
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-06-08 15:40:02 +0300

    Show Page

--- resources/views/livewire/recently-reviewed.blade.php
+++ resources/views/livewire/recently-reviewed.blade.php
@@ -2,7 +2,7 @@
     @forelse ($recentlyReviewed as $game)
     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
         <div class="relative flex-none">
-            <a href="#">
+            <a href="{{ route('games.show', $game['slug']) }}">
                 <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="game cover"
                     class="w-48 hover:opacity-75 transition ease-in-out duration-150">
             </a>
@@ -14,7 +14,7 @@
             </div>
         </div>
         <div class="ml-6 lg:ml-12">
-            <a href="#"
+            <a href="{{ route('games.show', $game['slug']) }}"
                 class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">{{ $game['name'] }}</a>
             <div class="text-gray-400 mt-1">
                 @foreach ($game['platforms'] as $platform)
@@ -29,6 +29,24 @@
         </div>
     </div> <!-- end game -->
     @empty
-    <div class="spinner mt-8"></div>
+    @foreach (range(1, 3) as $game)
+    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+        <div class="relative flex-none">
+            <div class="bg-gray-700 w-32 lg:w-48 h-40 lg:h-56"></div>
+        </div>
+        <div class="ml-6 lg:ml-12">
+            <div class="inline-block text-lg font-semibold leading-tight text-transparent bg-gray-700 rounded mt-4">
+                Title goes here for game</div>
+            <div class="mt-8 space-y-4 hidden lg:block">
+                <span class="text-transparent bg-gray-700 rounded inline-block">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Lorem ipsum.</span>
+                <span class="text-transparent bg-gray-700 rounded inline-block">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Lorem ipsum.</span>
+                <span class="text-transparent bg-gray-700 rounded inline-block">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Lorem ipsum.</span>
+            </div>
+        </div>
+    </div> <!-- end game -->
+    @endforeach
     @endforelse
 </div>
