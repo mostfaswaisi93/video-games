f70aad071192abb201774a7926426e97e8b6b976
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-06-08 15:19:00 +0300

    Use API Key

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/', 'GamesController@index')->name('games.index');
+
+Route::get('/show', function () {
+    return view('show');
 });
